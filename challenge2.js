f78c6e181f7127a761de707f1e40dc9f0c0f41b6
Rachel Brendel <brendelrachel@gmail.com>
2023-01-13 14:30:28 -0600

    add challenge2.js

---
+++ challenge2.js
@@ -0,0 +1,6 @@
+//Goal of this challenge is to change the string to all lower case
+// create a var with a string
+// change font to all lower case using .toLowerCase
+
+var welcomeSaying = "HeLlo, STrangER!!"
+console.log(welcomeSaying.toLowerCase())
