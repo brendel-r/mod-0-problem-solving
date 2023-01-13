01d389bb3c0978f1994e66724e4e19034b293fd5
Rachel Brendel <brendelrachel@gmail.com>
2023-01-13 17:16:39 -0600

    revise  challenge2.js file

--- challenge2.js
+++ challenge2.js
@@ -1,6 +1,9 @@
-//Goal of this challenge is to change the string to all lower case
+//Goal of this challenge is to change the array of strings to all lower case
 // create a var with a string
 // change font to all lower case using .toLowerCase
 
-var welcomeSaying = "HeLlo, STrangER!!"
-console.log(welcomeSaying.toLowerCase())
+var words = ["HeLlo", "STrangER", "RuNNing"]
+
+for (var i = 0; i < words.length; i++) {
+console.log(words[i].toLowerCase());
+}
